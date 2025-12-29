f261c8737a8f315a6a86d552a2acebac12b2aae1
k-sub1995 <k-sub1995@users.noreply.github.com>
2025-12-29 15:12:31 +0900

    refactor: routers directory name
    endpointsからroutersに修正。
    公式がroutes/だったが、routers/が一般的と考えたため修正。

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import items
+from app.api.v1.routers import items
 
 api_router = APIRouter()
 api_router.include_router(items.router, prefix="/items", tags=["items"])
